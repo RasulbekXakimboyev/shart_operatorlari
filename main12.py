b85960580fc5f4de0beb73d227ea8f06d1ac6beb
Rasulbek Xakimbayev <rasulbekxakimbayev@gmail.com>
2025-11-10 15:28:00 +0500

    Add files via upload

---
+++ main12.py
@@ -0,0 +1,270 @@
+## 1
+# student_grades = {"Matematika": 90, "Fizika": 85, "Informatika" : 95 }
+# student_grades["Kimyo"] = 88
+# result1 = student_grades
+# print("1-vazifa natijasi:", result1)
+# student_grades["Fizika"] = 87
+# result2 = student_grades
+# print("2-vazifa natijasi:", result2)
+# del student_grades["Informatika"]
+# result3 = student_grades
+# print("3-vazifa natijasi:", result3)
+# result4 = sum(student_grades.values())
+# print("4-vazifa natijasi:", result4)
+# result5 = sum(student_grades.values()) / len(student_grades)
+# print("5-vazifa natijasi:", result5)
+# result6 = max(student_grades.values())
+# print("6-vazifa natijasi:", result6)
+# result7 = min(student_grades.values())
+# print("7-vazifa natijasi:", result7)
+# result8 = list(student_grades.keys())
+# print("8-vazifa natijasi:", result8)
+# result9 = list(student_grades.values())
+# print("9-vazifa natijasi:", result9)
+# result10 = len(student_grades) == 0
+# print("10-vazifa natijasi:", result10)
+
+
+## 2
+# fruits = {"olma": 5000, "banan": 12000, "uzum": 8000}
+# fruits["anor"] = 10000
+# result1 = fruits
+# print("1-vazifa natijasi:", result1)
+# fruits["banan"] = 15000
+# result2 = fruits
+# print("2-vazifa natijasi:", result2)
+# del fruits["uzum"]
+# result3 = fruits
+# print("3-vazifa natijasi:", result3)
+# result4 = sum(fruits.values())
+# print("4-vazifa natijasi:", result4)
+# result5 = max(fruits, key=fruits.get)
+# print("5-vazifa natijasi:", result5)
+# result6 = min(fruits, key=fruits.get)
+# print("6-vazifa natijasi:", result6)
+# result7 = sorted(fruits.values())
+# print("7-vazifa natijasi:", result7)
+# result8 = len(fruits)
+# print("8-vazifa natijasi:", result8)
+# result9 = fruits.get("olma")
+# print("9-vazifa natijasi:", result9)
+# result10 = sorted(fruits.keys())
+# print("10-vazifa natijasi:", result10)
+
+
+## 3
+# cities = {"Toshkent": 2500000, "Samarqand": 550000, "Buxoro": 280000}
+# cities["Andijon"] = 600000
+# r = cities
+# print("1-vazifani natijasi",r)
+# cities["Toshkent"] = 2600000
+# r2 = cities
+# print("2-vazifani natijasi",r2)
+# del cities["Buxoro"]
+# r3 = cities
+# print("3-vazifani natijasi",)
+# r4 = sum(cities.values()) / len(cities)
+# print("4-vazifani natijasi",r4)
+# r5 = max(cities, key=cities.get)
+# print("5-vazifani natijasi",r5)
+# r6 = min(cities, key=cities.get)
+# print("6-vazifani natijasi",r6)
+# r7 = sorted(cities.values())
+# print("7-vazifani natijasi",r7)
+# r8 = len(cities)
+# print("8-vazifani natijasi",r8)
+# r9 = list(cities.keys())
+# print("9-vazifani natijasi",r9)
+# r10 = len(cities) == 0
+# print("10-vazifani natijasi",r10)
+
+
+## 4
+# books = {"Alximik": "Paulo Koelyo", "Shaytanat": "Tohir Malik", "1984": "George Orwell"}
+# books["O‘tkan kunlar"] = "Abdulla Qodiriy"
+# r1 = books
+# print("1-vazifani natijasi",r1)
+# books["Alximik"] = "Paulo Coelho"
+# r2  = books
+# print("2-vazifani natijasi",r2)
+# del books[1984]
+# r3 = books
+# print("3-vazifani natijasi",r3)
+# r4 = len(books)
+# print("4-vazifani natijasi",r4)
+# r5 = books.values()
+# print("5-vazifani natijasi",r5)
+# r6 = sorted(books.keys())
+# print("6-vazifani natijasi",r6)
+# r7 = books.get("Shaytanat")
+# print("7-vazifani natijasi",r7)
+# r8 = len(books)
+# print("8-vazifani natijasi",r8)
+# if "O'tkan kunlar" in books:
+#     print("O'tkan kunlar bor!")
+#
+# else:
+#     print("O'tkan kunlar yo'q!")
+# books.clear()
+# print(f"Tozalanggan dictionary: {books}")
+
+## 5
+# currencies = {"USD": 12600, "EUR": 13500, "RUB": 140}
+# currencies["GBP"] = 16000
+# r1 = currencies
+# print("1-vazifani natijasi",r1)
+# currencies["USD"] = 12700
+# r2  = currencies
+# print("2-vazifani natijasi",r2)
+# del currencies["RUB"]
+# r3 = currencies
+# print("3-vazifani natijasi",r3)
+# r4 = sum(currencies.values())
+# print("4-vazifani natijasi",r4)
+# r5 = max(currencies, key=currencies.get)
+# print("5-vazifani natijasi",r5)
+# r6 = min(currencies, key=currencies.get)
+# print("6-vazifani natijasi",r6)
+# r7 = list(currencies.keys())
+# print("7-vazifani natijasi",r7)
+# r8 = sorted(currencies.values())
+# print("8-vazifani natijasi",r8)
+# r9 = currencies.get("EUR")
+# print("9-vazifani natijasi",r9)
+# r10 = len(currencies) == 0
+# print("10-vazifa natijasi:",r10)
+
+
+# # 6
+# inventory = {"suv": 50, "non": 100, "shokolad": 20}
+# inventory["cola"] = 30
+# inventory1 = inventory
+# print("1-vazifa natijasi:", inventory1)
+# inventory["non"] = 80
+# inventory2 = inventory
+# print("2-vazifa natijasi:", inventory2)
+# del inventory["shokolod"]
+# inventory3 = inventory
+# print("3-vazifa natijasi:", inventory3)
+# inventory4 = sum(inventory.values())
+# print("4-vazifa natijasi:", inventory4)
+# inventory5= max(inventory, key=inventory.get)
+# print("5-vazifa natijasi:", inventory5)
+# inventory6 = min(inventory, key=inventory.get)
+# print("6-vazifa natijasi:", inventory6)
+# inventory7 = list(inventory.keys())
+# print("7-vazifa natijasi:", inventory7)
+# inventory8 = sorted(inventory.values())
+# print("8-vazifa natijasi:", inventory8)
+# inventory9 = inventory.get("EUR")
+# print("9-vazifa natijasi:", inventory9)
+# inventory10 = len(inventory) == 0
+# print("10-vazifa natijasi:", inventory10)
+
+
+## 7
+# athletes = {"Ali": 150, "Vali": 120, "Sardor": 180}
+# athletes["Nodir"] = 130
+# athletes1 = athletes
+# print("1-vazifa natijasi:", athletes1)
+# athletes["vali"] = 140
+# athletes2 = athletes
+# print("2-vazifa natijasi:", athletes2)
+# del athletes["Sardor"]
+# athletes3 = athletes
+# print("3-vazifa natijasi:", athletes3)
+# athletes4 = sum(athletes.values())
+# print("4-vazifa natijasi:", athletes4)
+# athletes5 = sum(athletes.values()) / len(athletes)
+# print("5-vazifa natijasi:", athletes5)
+# athletes6 = max(athletes, key=athletes.get)
+# print("6-vazifa natijasi:", athletes6)
+# athletes7 = min(athletes, key=athletes.get)
+# print("7-vazifa natijasi:", athletes7)
+# athletes8 = list(athletes.keys())
+# print("8-vazifa natijasi:", athletes8)
+# athletes9 = sorted(athletes.values())
+# print("9-vazifa natijasi:", athletes9)
+# athletes10 = len(athletes) == 0
+# print("10-vazifa natijasi:", athletes10)
+
+
+## 8
+# menu = {"osh": 25000, "shashlik": 30000, "somsa": 5000}
+# menu["lag'mon"] = 20000
+# menu1 = menu
+# print("1-vazifa natijasi:", menu1)
+# menu["somsa"] = 6000
+# menu2 = menu
+# print("2-vazifa natijasi:", menu2)
+# del menu["shashlik"]
+# menu3 = menu
+# print("3-vazifa natijasi:", menu3)
+# menu4 = sum(menu.values())
+# print("4-vazifa natijasi:", menu4)
+# menu5 = max(menu, key=menu.get)
+# print("5-vazifa natijasi:", menu5)
+# menu6 = min(menu, key=menu.get)
+# print("6-vazifa natijasi:", menu6)
+# menu7 = list(menu.keys())
+# print("7-vazifa natijasi:", menu7)
+# menu8 = sorted(menu.values())
+# print("8-vazifa natijasi:", menu8)
+# menu9  = menu.get("osh")
+# print("9-vazifa natijasi:", menu9)
+# menu10 = len(menu) == 0
+# print("10-vazifa natijasi:", menu10)
+
+
+## 9
+# faculties = {"IT": 120, "Matematika": 80, "Filologiya": 60}
+# faculties["Fizika"] = 90
+# faculties1 = faculties
+# print("1-vazifa natijasi:", faculties1)
+# faculties["IT"] = 130
+# faculties2 = faculties
+# print("2-vazifa natijasi:", faculties2)
+# del faculties["Filologiya"]
+# faculties3 = faculties
+# print("3-vazifa natijasi:", faculties3)
+# faculties4 = sum(faculties.values())
+# print("4-vazifa natijasi:", faculties4)
+# faculties5 = sum(faculties.values()) / len(faculties)
+# print("5-vazifa natijasi:", faculties5)
+# faculties6 = max(faculties, key=faculties.get)
+# print("6-vazifa natijasi:", faculties6)
+# faculties7 = min(faculties, key=faculties.get)
+# print("7-vazifa natijasi:", faculties7)
+# faculties8 = list(faculties.keys())
+# print("8-vazifa natijasi:", faculties8)
+# faculties9 = sorted(faculties.values())
+# print("9-vazifa natijasi:", faculties9)
+# faculties10 = len(faculties) == 0
+# print("10-vazifa natijasi:", faculties10)
+
+
+## 10
+# destinations = {"Parij": 500, "Dubay": 300, "Istanbul": 200}
+# destinations["London"] = 450
+# destinations1 = destinations
+# print("1-vazifa natijasi:", destinations1)
+# destinations["Istanbul"] = 250
+# destinations2 = destinations
+# print("2-vazifa natijasi:", destinations2)
+# del destinations["Dubay"]
+# destinations3 = destinations
+# print("3-vazifa natijasi:", destinations3)
+# destinations4 = sum(destinations.values())
+# print("4-vazifa natijasi:", destinations4)
+# destinations5 = max(destinations, key=destinations.get)
+# print("5-vazifa natijasi:", destinations5)
+# destinations6 = min(destinations, key=destinations.get)
+# print("6-vazifa natijasi:", destinations6)
+# destinations7 = list(faculties.keys())
+# print("7-vazifa natijasi:", destinations7)
+# destinations8 = sorted(destinations.values())
+# print("8-vazifa natijasi:", destinations8)
+# destinations9 = destinations.get("Parij")
+# print("9-vazifa natijasi:", destinations9)
+# destinations10 = len(destinations) == 0
+# print("10-vazifa natijasi:", destinations10)
